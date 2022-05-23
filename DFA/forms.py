d42d4269b313700ac7561f7be2a60563e3527030
Kok Bai Yuan <baiyuan100722@student.usm.my>
2022-05-23 16:47:07 +0800

    Added placeholder & style

--- DFA/forms.py
+++ DFA/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class DFAForm(forms.Form):
-    testStrings = forms.CharField(widget=forms.Textarea(attrs={'name':'testStrings', 'rows':'10', 'cols':'100'}))
+    testStrings = forms.CharField(widget=forms.Textarea(attrs={'name':'testStrings', 'rows':'10', 'cols':'100', 'placeholder': 'Insert text here...', 'style': 'border-radius: 1em;'}))
