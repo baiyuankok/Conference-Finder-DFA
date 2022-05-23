dcd48a74bfd7307ec9baa82125483932c241de05
Kok Bai Yuan <baiyuan100722@student.usm.my>
2022-05-23 17:56:15 +0800

    Updated placeholder value

--- DFA/forms.py
+++ DFA/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class DFAForm(forms.Form):
-    testStrings = forms.CharField(widget=forms.Textarea(attrs={'name':'testStrings', 'rows':'10', 'cols':'100', 'placeholder': 'Insert text here...', 'style': 'border-radius: 1em;'}))
+    testStrings = forms.CharField(widget=forms.Textarea(attrs={'name':'testStrings', 'rows':'10', 'cols':'100', 'placeholder': 'Patterns accepted are ICMBS, ICITS, ICIRPEBS, ICBAES, ICBMSS, ICEABT, ICEFR, ICCSNS, ICCSPEE, ICCSIEE, ICCBEE, ICAASS, ICAPM, ICAIMLBDE, ICAIRME, EUICNB, EUIC3BCB, EUIWNMC, EUCELS', 'style': 'border-radius: 1em;'}))
